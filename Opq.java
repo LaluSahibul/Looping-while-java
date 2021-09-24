bf8f875112c0df8f84e41e3b945f2d6f7c8b0a53
LaluSahibul <lalu113ul@gmail.com>
2021-09-24 14:01:07 -0900

    loop-whle

---
+++ Opq.java
@@ -0,0 +1,34 @@
+package loop_W;
+import java.util.Scanner;
+public class Opq {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		boolean a=true;
+		String nama;
+		Scanner x=new Scanner (System.in);
+		
+		while (a) {
+			System.out.print("masukkan nama>> ");
+			nama=x.nextLine();
+			if(nama.contentEquals("baqi hofian andriana")||nama.contentEquals("Baqi Hofian Andriana")||nama.contentEquals("opq")||nama.contentEquals("OPQ")) {
+				System.out.println("\n");
+				System.out.println("       ******   ******       ");
+				System.out.println("      ******** ********      ");
+				System.out.println("      ********L********      ");
+				System.out.println("      ********O********      ");
+				System.out.println("       *******V*******       ");
+				System.out.println("        ******E******        ");
+				System.out.println("          *********          ");
+				System.out.println("            *****            ");
+				System.out.println("              *              ");
+				System.out.println("                             ");
+				a=false;
+			}else {
+				System.out.println(nama+" siapa?");
+				a=true;
+		}
+	}
+
+}
+}
